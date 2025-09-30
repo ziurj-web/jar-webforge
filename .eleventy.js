aee375abd97b5d77d670beebed73593b9a50c15e
jruiz <jruiz@disroot.org>
2025-09-30 13:55:05 -0400

    idfk: Have to save

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+    // Things we're going to need.
     eleventyConfig.addPassthroughCopy("./src/styles.css");
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/script.js");
     
+    // Date filter for blog posts.
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    });
+
     return {
         dir: {
             input: "src", // Where we're working out of
